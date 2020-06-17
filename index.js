4480af3ba4078b94a05a789fe20c78a06d1e72cd
ramenguy99 <ramenguy99@gmail.com>
2020-06-17 18:04:13 +0200

    Basic server

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 http.createServer(
-function (request, response) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end('<h1>Hello World</h1>');
+function (req, res) {
+    var q = url.parse(req.url, true);
+    switch (q.pathname) {
+        case "/player.html":
+            fs.readFile("player.html", function(err, data) {
+                if(err) {
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    return res.end("<h1>404 Not Found</h1>");
+                }
+                
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+            break;
+        
+        default:
+            fs.readFile("." + q.pathname, function(err, data) {
+                if(err) {
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    return res.end("<h1>404 Not Found</h1>");
+                }
+                
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+            break;
+    }
 }).listen(8000);
