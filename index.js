f9de614ee666cc4f5f9e71471d86909d191c0436
ramenguy99 <ramenguy99@gmail.com>
2020-06-18 11:13:40 +0200

    Public e stories con ajax

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
-var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = 8000;
 
-http.createServer(
-function (req, res) {
-    var q = url.parse(req.url, true);
-    switch (q.pathname) {
-        case "/player.html":
-            fs.readFile("player.html", function(err, data) {
-                if(err) {
-                    res.writeHead(404, {'Content-Type': 'text/html'});
-                    return res.end("<h1>404 Not Found</h1>");
-                }
-                
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                return res.end();
+app.get('/stories', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    fs.readdir('stories', function(err, files) {
+        let stories = [];
+        if(!err) {            
+            files.forEach(function(f) {
+                name = path.parse(f).name;
+                stories.push(name);
             });
-            break;
+        }
+        res.write(JSON.stringify(stories));
+        res.end();
+    });
+});
+
+app.get('/player', function(req, res) {
+    fs.readFile("player.html", function(err, data) {
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("<h1>404 Not Found</h1>");
+        }
         
-        default:
-            fs.readFile("." + q.pathname, function(err, data) {
-                if(err) {
-                    res.writeHead(404, {'Content-Type': 'text/html'});
-                    return res.end("<h1>404 Not Found</h1>");
-                }
-                
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                return res.end();
-            });
-            break;
-    }
-}).listen(8000);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+});
+
+app.get('/author', function(req, res) {
+    fs.readFile("author.html", function(err, data) {
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("<h1>404 Not Found</h1>");
+        }
+        
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+});
+
+app.get('/evaluator', function(req, res) {
+    fs.readFile("evaluator.html", function(err, data) {
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("<h1>404 Not Found</h1>");
+        }
+        
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+});
+
+app.use(express.static('public'));
+
+app.use(function(req, res){
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    return res.end("<h1>404 Not Found</h1>");
+});
+
+app.listen(port, function() {});
+
