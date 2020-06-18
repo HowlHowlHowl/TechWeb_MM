168c863271055036db697843420f3996963925dd
ramenguy99 <ramenguy99@gmail.com>
2020-06-18 18:36:05 +0200

    Author story api

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ var express = require('express');
 var app = express();
 var port = 8000;
 
+var nextStoryId = 0;
+
+//Fa parsing di body json nelle richieste che specificano application/json come content type
+app.use(express.json());
+
+//Story REST API
 app.get('/stories', function(req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
     fs.readdir('stories', function(err, files) {
         let stories = [];
         if(!err) {            
             files.forEach(function(f) {
-                name = path.parse(f).name;
-                stories.push(name);
+                let data = JSON.parse(fs.readFileSync('stories/' + f));
+                let story = {};
+                story.name = data.name;
+                story.id = data.id;
+                story.accessible = data.accessible;
+                story.published = data.published;
+                stories.push(story);
             });
         }
         res.write(JSON.stringify(stories));
@@ -20,6 +31,122 @@ app.get('/stories', function(req, res) {
     });
 });
 
+function checkStory(s)
+{
+    return true;
+}
+
+//Create new story
+app.post('/stories', function(req, res) {
+    let story = req.body;
+    if(checkStory(story))
+    {
+        new_id = nextStoryId++;
+        story.id = new_id;
+        fs.writeFile("stories/story" + new_id + ".json", JSON.stringify(story), function(err) {
+            if(!err) {
+                res.status(200).send();
+            } else {
+                res.status(500).send();
+            }
+        });
+    } else {
+        res.status(400).send();
+    }
+});
+
+//Update existing story
+app.put('/stories/:id', function(req, res) {
+    let story = req.body;
+    let id = req.params.id;
+    if(checkStory(story)) {
+        let path = "stories/story" + id + ".json";
+        if(fs.existsSync(path)) {
+            fs.writeFile(path, JSON.stringify(story), function(err) {
+                if(!err) {
+                    res.status(200).send();
+                } else {
+                    res.status(500).send();
+                }
+            });
+        } else {
+            res.status(400).send();
+        }
+    } else {
+        res.status(400).send();
+    }
+});
+
+//Duplicate, archive, publish, delete
+app.post('/stories/:id', function(req, res) {
+    let id = req.params.id;
+    let path = "stories/story" + id + ".json";
+    
+    if(fs.existsSync(path)) {
+        let action = req.body.action;
+        switch(action) {
+            case "duplicate":
+                fs.readFile(path, (err, data) => {
+                    if(err) {
+                        res.status(500).send();
+                    } else {
+                        let new_id = nextStoryId++;
+                        let path = "stories/story" + new_id + ".json";
+                        fs.writeFile(path, data, function(err) {
+                            res.status(err ? 500 : 200).send();
+                        });
+                    }
+                });
+                break;
+                
+            case "archive":
+            case "publish":
+                fs.readFile(path, (err, data) => {
+                    if(err) {
+                        res.status(500).send();
+                    } else {
+                        let story = JSON.parse(data);
+                        let path = "stories/story" + id + ".json";
+                        
+                        story.published = (action == "publish") ? true : false;
+                        fs.writeFile(path, JSON.stringify(story), function(err) {
+                            res.status(err ? 500 : 200).send();
+                        });
+                    }
+                });
+                break;
+                
+            case "delete":
+                fs.unlink(path, (err) => {
+                    res.status(err ? 500 : 200).send();
+                });
+                break;
+            
+            default:
+                res.status(400).send();
+                break;
+        }
+    } else { 
+        res.status(400).send();
+    }
+});
+
+//Get story by id
+app.post('/stories/:id', function(req, res) {
+    let id = req.params.id;
+    let path = "stories/story" + id + ".json";
+    readFile(path, (err, data) => {
+        if(!err) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(data);
+            return res.end();
+        } else {
+            res.status(400).send();
+        }
+    });
+}
+
+//Ambiente player
 app.get('/player', function(req, res) {
     fs.readFile("player.html", function(err, data) {
         if(err) {
@@ -33,6 +160,7 @@ app.get('/player', function(req, res) {
     });
 });
 
+//Ambiente author
 app.get('/author', function(req, res) {
     fs.readFile("author.html", function(err, data) {
         if(err) {
@@ -46,6 +174,7 @@ app.get('/author', function(req, res) {
     });
 });
 
+//Ambiente evaluator
 app.get('/evaluator', function(req, res) {
     fs.readFile("evaluator.html", function(err, data) {
         if(err) {
@@ -59,11 +188,22 @@ app.get('/evaluator', function(req, res) {
     });
 });
 
+//Rende disponibili tutti i file nella directory public e nelle sue subdirectory
 app.use(express.static('public'));
 
+//Handler in caso di richieste inesistenti
 app.use(function(req, res){
     res.writeHead(404, {'Content-Type': 'text/html'});
     return res.end("<h1>404 Not Found</h1>");
+});
+
+//Aggiorna la nuova nextStoryId
+fs.readdirSync('stories').forEach((f) => {
+    matches = f.match(/(\d+)/);
+    if(matches)
+    {
+        nextStoryId = Math.max(nextStoryId, Number(matches[0]) + 1);
+    }
 });
 
 app.listen(port, function() {});
