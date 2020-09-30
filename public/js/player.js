5f20ec8c37503b5e754e21d6bc0b4edfa2b9b16a
Korobyu-san <64589280+Korobyu-san@users.noreply.github.com>
2020-09-30 13:27:24 +0200

    stili default e altri senza background image perchè non va

--- public/js/player.js
+++ public/js/player.js
@@ -734,7 +734,7 @@ function convertHex(hex,opacity){
     let r = parseInt(hex.substring(0,2), 16);
     let g = parseInt(hex.substring(2,4), 16);
     let b = parseInt(hex.substring(4,6), 16);
-    
+    console.log('rgba(' + r + ',' + g + ',' + b + ',' + opacity + ')');
     return 'rgba(' + r + ',' + g + ',' + b + ',' + opacity + ')';
 }
 
